42155b6fe0b85dac693efb40743b277ddd8241dd
Nesta Parchment <saint.clever.np@gmail.com>
2021-01-10 16:15:06 -0500

    gui layout

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 
@@ -7,3 +9,26 @@
 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title('PyPass: password manager')
+window.config(padx=20, pady=20)
+window.resizable(False, False)
+
+
+canvas = Canvas(width=200, height=200, highlightthickness=0)
+
+# bg_img = PhotoImage(file='assets/bg_00.png')
+# canvas.create_image(100, 100, image=bg_img)
+# canvas.grid(column=0, row=0)
+
+logo_img = PhotoImage(file='assets/logo.png')
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(column=1, row=1)
+
+
+
+l = Label(text='hello')
+l.grid(column=1, row=1)
+
+
+window.mainloop()
