51d03d3911b0a6927852cb4aefe85686ce63d5dc
Nesta Parchment <saint.clever.np@gmail.com>
2021-01-10 22:15:04 -0500

    create password generator

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
 from tkinter import *
+from tkinter import messagebox
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+# Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+nr_letters = random.randint(8, 10)
+nr_symbols = random.randint(2, 4)
+nr_numbers = random.randint(2, 4)
+
+password_list = []
+
+for char in range(nr_letters):
+  password_list.append(random.choice(letters))
+
+for char in range(nr_symbols):
+  password_list += random.choice(symbols)
+
+for char in range(nr_numbers):
+  password_list += random.choice(numbers)
+
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Your password is: {password}")
 
 
 
@@ -10,12 +39,20 @@ def save_password():
     email = email_username_entry.get()
     password = password_entry.get()
 
-    with open('data.txt', 'a') as file:
-        file.write(f'{website} | {email} | {password}\n')
+    if website == '' or email == '' or password == '':
+        messagebox.showinfo(title='Oops', message='OopsEmpty fields present')
+    else:
 
-    website_entry.delete(0, END)
-    # email_username_entry.delete(0, END) # I rather save my email
-    password_entry.delete(0, END)
+        # message box
+        is_ok = messagebox.askokcancel(title=website, message=f'Details entered:\n website: {website}\n Email: {email}\n Password: {password}\n\n Is it ok to save?')
+
+        if is_ok:
+            with open('data.txt', 'a') as file:
+                file.write(f'{website} | {email} | {password}\n')
+
+                website_entry.delete(0, END)
+                # email_username_entry.delete(0, END) # I rather save my email
+                password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
